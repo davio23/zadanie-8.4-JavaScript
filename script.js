e94c768e60d39cd7b4d53d664accaf717197be60
davio23 <33763332+davio23@users.noreply.github.com>
2017-11-24 17:28:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Alert. It's the first line of JavaScript 
 code var name = prompt('Enter your name');
-// The second line of JavaScript code.
-alert('Hello, ' + name);
+// The second line of JavaScript code. alert('Hello, ' + name)
 //Below there is the last line of JavaScript code.
 console.log('Hello, ' + name);
